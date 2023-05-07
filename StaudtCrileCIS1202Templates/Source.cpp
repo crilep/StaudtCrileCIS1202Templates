3e9a570f9be8b06deb76655bad0ed2fdf8986f19
unlik <unlik@DESKTOP-463LAC8>
2023-05-06 20:59:07 -0400

    Add project files.

---
+++ StaudtCrileCIS1202Templates/Source.cpp
@@ -0,0 +1,32 @@
+//Crile Staudt
+//CIS 1202 101
+//May 7, 2023
+
+#include<iostream>
+
+
+using namespace std;
+
+template <class T>
+T Half(T);
+
+int Half(int);
+
+int main() {
+
+	double a;
+	float b;
+	int c;
+
+
+
+
+}
+
+int Half(int integer) {
+
+	int intFinal;
+
+	return intFinal;
+
+}
